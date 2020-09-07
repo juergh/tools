a8435a3255ed559eaf79927cd2a744a223bfc082
Juerg Haefliger <juergh@gmail.com>
2020-09-07 11:39:34 +0200

    rpi/create-list: Add an exit handler
    Signed-off-by: Juerg Haefliger <juergh@gmail.com>

--- rpi/create-list.sh
+++ rpi/create-list.sh
@@ -1,9 +1,20 @@
-#!/bin/bash -u
+#!/bin/bash -eu
+
+function out()
+{
+	local rc=$?
+
+	if [ "${rc}" -ne 0 ] ; then
+		echo "Script failed" >&2
+	fi
+}
 
 function encode_line()
 {
 	echo "__MARK__${STATUS}__MARK__${COUNT}__MARK__${COMMIT}__MARK__${PATCH_ID}__MARK__${SUBJECT}__MARK__${FUZZY_SUBJECT}__MARK__"
 }
+
+trap out EXIT INT TERM HUP
 
 COUNT=0
 while IFS= read -r line ; do
