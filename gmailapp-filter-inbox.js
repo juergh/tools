c9892a7f3241d87793df99d8d31671b55dbe51e8
Juerg Haefliger <juergh@proton.me>
2026-06-18 16:44:06 +0200

    gmailapp-filter-inbox.js: Batch operations
    Lot of API calls which can easily put us over the daily quota. Batch
    the calls to (try to) prevent that.
    
    Signed-off-by: Juerg Haefliger <juergh@proton.me>

--- gmailapp-filter-inbox.js
+++ gmailapp-filter-inbox.js
@@ -11,67 +11,74 @@ function filter_inbox() {
     // Filter rules
     const FILTERS = [
         // Trash
-        ["subject.includes(' abi-testing: ABI testing report')",      "Trash"],
-        ["subject.includes(' -proposed tracker')",                    "Trash"],
-        ["subject.includes(' Workflow done!')",                       "Trash"],
-        ["subject.includes(' uploaded (ABI bump)')",                  "Trash"],
-        ["subject.includes(' Live Kernel Patching release tracker')", "Trash"],
-        ["subject.includes(' build of livepatch-linux-')",            "Trash"],
+        [({subject}) => subject.includes(' abi-testing: ABI testing report'),      "Trash"],
+        [({subject}) => subject.includes(' -proposed tracker'),                    "Trash"],
+        [({subject}) => subject.includes(' Workflow done!'),                       "Trash"],
+        [({subject}) => subject.includes(' uploaded (ABI bump)'),                  "Trash"],
+        [({subject}) => subject.includes(' Live Kernel Patching release tracker'), "Trash"],
+        [({subject}) => subject.includes(' build of livepatch-linux-'),            "Trash"],
 
         // Pre-filter
-        ["subject.includes('The Daily Bug Report for 20')",         "Canonical/Bugs"],
-        ["subject.includes('SFDC')",                                "Canonical/SalesForce"],
-        ["body.includes('Launchpad-Subscription: linux-firmware')", "Launchpad-Message-For/juergh"],
+        [({subject}) => subject.includes('The Daily Bug Report for 20'),         "Canonical/Bugs"],
+        [({subject}) => subject.includes('SFDC'),                                "Canonical/SalesForce"],
+        [({body}) => body.includes('Launchpad-Subscription: linux-firmware'),    "Launchpad-Message-For/juergh"],
 
         // From
-        ["from.includes('noreply+ckctreview-bot@canonical.com')", "Canonical/Bots"],
-        ["from.includes('prod-kernel-team-janitor')",             "Canonical/Bots"],
-        ["from.includes('noreply+forgejo-bot@canonical.com')",    "Canonical/Forgejo"],
-        ["from.includes('jira@warthogs.atlassian.net')",          "Canonical/Jira"],
+        [({from}) => from.includes('noreply+ckctreview-bot@canonical.com'), "Canonical/Bots"],
+        [({from}) => from.includes('prod-kernel-team-janitor'),             "Canonical/Bots"],
+        [({from}) => from.includes('noreply+forgejo-bot@canonical.com'),    "Canonical/Forgejo"],
+        [({from}) => from.includes('jira@warthogs.atlassian.net'),          "Canonical/Jira"],
 
         // List-Id (Canonical)
-        ["list_id.includes('canonical.github.com')",                    "Canonical/Github"],
-        ["list_id.includes('discourse.canonical.com')",                 "Canonical/Discourse"],
-        ["list_id.includes('kernel-esm-reviews.groups.canonical.com')", "Mailing List/Canonical/canonical-kernel-esm"],
-        ["list_id.includes('warthogs.lists.canonical.com')",            "Mailing List/Canonical/warthogs"],
+        [({list_id}) => list_id.includes('canonical.github.com'),                    "Canonical/Github"],
+        [({list_id}) => list_id.includes('discourse.canonical.com'),                 "Canonical/Discourse"],
+        [({list_id}) => list_id.includes('kernel-esm-reviews.groups.canonical.com'), "Mailing List/Canonical/canonical-kernel-esm"],
+        [({list_id}) => list_id.includes('warthogs.lists.canonical.com'),            "Mailing List/Canonical/warthogs"],
 
         // List-Id (Ubuntu)
-        ["list_id.includes('discourse.ubuntu.com')",               "Canonical/Discourse"],
-        ["list_id.includes('devel-permissions.lists.ubuntu.com')", "Mailing List/Ubuntu/devel-permissions"],
-        ["list_id.includes('kernel-team.lists.ubuntu.com')",       "Mailing List/Ubuntu/kernel-team"],
-        ["list_id.includes('ubuntu-devel.lists.ubuntu.com')",      "Mailing List/Ubuntu/ubuntu-devel"],
-        ["list_id.includes('ubuntu-release.lists.ubuntu.com')",    "Mailing List/Ubuntu/ubuntu-release"],
+        [({list_id}) => list_id.includes('discourse.ubuntu.com'),               "Canonical/Discourse"],
+        [({list_id}) => list_id.includes('devel-permissions.lists.ubuntu.com'), "Mailing List/Ubuntu/devel-permissions"],
+        [({list_id}) => list_id.includes('kernel-team.lists.ubuntu.com'),       "Mailing List/Ubuntu/kernel-team"],
+        [({list_id}) => list_id.includes('ubuntu-devel.lists.ubuntu.com'),      "Mailing List/Ubuntu/ubuntu-devel"],
+        [({list_id}) => list_id.includes('ubuntu-release.lists.ubuntu.com'),    "Mailing List/Ubuntu/ubuntu-release"],
 
         // Body
-        ["body.includes('Launchpad-Message-For: canonical-kernel-crankers')",      "Launchpad-Message-For/canonical-kernel-crankers"],
-        ["body.includes('Launchpad-Message-For: canonical-kernel-esm')",           "Launchpad-Message-For/canonical-kernel-esm"],
-        ["body.includes('Launchpad-Message-For: canonical-kernel-private')",       "Launchpad-Message-For/canonical-kernel-private"],
-        ["body.includes('Launchpad-Message-For: canonical-kernel-rt')",            "Launchpad-Message-For/canonical-kernel-rt"],
-        ["body.includes('Launchpad-Message-For: canonical-kernel-security-team')", "Launchpad-Message-For/canonical-kernel-security-team"],
-        ["body.includes('Launchpad-Message-For: canonical-kernel-team')",          "Launchpad-Message-For/canonical-kernel-team"],
-        ["body.includes('Launchpad-Message-For: canonical-livepatch-kernel')",     "Launchpad-Message-For/canonical-livepatch-kernel"],
-        ["body.includes('Launchpad-Message-For: juergh')",                         "Launchpad-Message-For/juergh"],
+        [({body}) => body.includes('Launchpad-Message-For: canonical-kernel-crankers'),      "Launchpad-Message-For/canonical-kernel-crankers"],
+        [({body}) => body.includes('Launchpad-Message-For: canonical-kernel-esm'),           "Launchpad-Message-For/canonical-kernel-esm"],
+        [({body}) => body.includes('Launchpad-Message-For: canonical-kernel-private'),       "Launchpad-Message-For/canonical-kernel-private"],
+        [({body}) => body.includes('Launchpad-Message-For: canonical-kernel-rt'),            "Launchpad-Message-For/canonical-kernel-rt"],
+        [({body}) => body.includes('Launchpad-Message-For: canonical-kernel-security-team'), "Launchpad-Message-For/canonical-kernel-security-team"],
+        [({body}) => body.includes('Launchpad-Message-For: canonical-kernel-team'),          "Launchpad-Message-For/canonical-kernel-team"],
+        [({body}) => body.includes('Launchpad-Message-For: canonical-livepatch-kernel'),     "Launchpad-Message-For/canonical-livepatch-kernel"],
+        [({body}) => body.includes('Launchpad-Message-For: juergh'),                         "Launchpad-Message-For/juergh"],
 
         // Post-filter
-        ["body.includes('Launchpad-Message-For: ')",                         "Launchpad-Message-For"],
-        ["to.includes('kernel-team-bot@canonical.com')",                     "Canonical/Bots"],
-        ["to.includes('kernel-team-bot+ancillary@canonical.com')",           "Canonical/Bots"],
-        ["to.includes('kernel-team-bot+ubuntu-kernel-gitea@canonical.com')", "Canonical/Bots"],
+        [({body}) => body.includes('Launchpad-Message-For: '),                         "Launchpad-Message-For"],
+        [({to}) => to.includes('kernel-team-bot@canonical.com'),                       "Canonical/Bots"],
+        [({to}) => to.includes('kernel-team-bot+ancillary@canonical.com'),             "Canonical/Bots"],
+        [({to}) => to.includes('kernel-team-bot+ubuntu-kernel-gitea@canonical.com'),   "Canonical/Bots"],
     ];
 
     /* --------------------------------------------------------------------------------------------------------------------------------- */
 
     // Get all user labels
-    var labels = {};
+    const labels = {};
     for (const label of GmailApp.getUserLabels()) {
         labels[label.getName()] = label
     }
 
-    // Walk through 100 messages with the filter label
+    // Batch buckets
+    const inboxThreads = [];
+    const trashThreads = [];
+    const labelThreads = {};
+    const removeLabelThreads = [];
+
+    // Walk through 100 messages with the filter label and classify
+    // Important: There's a 100-thread cap for addToThreads / removeFromThreads
     for (const thread of GmailApp.search(`label:"${FILTER_LABEL}"`, 0, 100)) {
+        removeLabelThreads.push(thread);
+
         if (thread.isInSpam()) {
-            // Ignore spam
-            thread.removeLabel(labels[FILTER_LABEL]);
             continue;
         }
 
@@ -80,14 +87,14 @@ function filter_inbox() {
         const from = message.getFrom().toLowerCase();
         const to = message.getTo().toLowerCase();
         const subject = message.getSubject();
-        const body = message.getPlainBody();
+        const body = message.getPlainBody() || "";
 
-        const list_id = message.getHeader("List-Id");
+        const list_id = message.getHeader("List-Id") || "";
 
         // Find the first filter rule that matches
-        var action = "Inbox";
+        let action = "Inbox";
         for (const filter of FILTERS) {
-            if (eval(filter[0])) {
+            if (filter[0]({from, to, subject, body, list_id})) {
                 action = filter[1];
                 break;
             }
@@ -97,17 +104,38 @@ function filter_inbox() {
 
         switch (action) {
         case "Inbox":
-            thread.moveToInbox();
+            inboxThreads.push(thread);
             break;
         case "Trash":
-            thread.moveToTrash();
+            trashThreads.push(thread);
             break;
         default:
-            thread.addLabel(labels[action]);
+            if (!labelThreads[action]) {
+                labelThreads[action] = [];
+            }
+            labelThreads[action].push(thread);
             break;
         }
 
-        // Last step: Remove the filter label
-        thread.removeLabel(labels[FILTER_LABEL]);
+    }
+
+    // Batch-apply actions
+    if (inboxThreads.length) {
+        GmailApp.moveThreadsToInbox(inboxThreads);
+    }
+    if (trashThreads.length) {
+        GmailApp.moveThreadsToTrash(trashThreads);
+    }
+    for (const [action, threads] of Object.entries(labelThreads)) {
+        if (!labels[action]) {
+            console.error(`Label not found: "${action}"`);
+            continue;
+        }
+        labels[action].addToThreads(threads);
+    }
+
+    // Batch-remove the filter label
+    if (removeLabelThreads.length) {
+        labels[FILTER_LABEL].removeFromThreads(removeLabelThreads);
     }
 }
